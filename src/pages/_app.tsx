bb62a2221a2e7c520410d30ce6976d35fcda6af1
Gabriel Tanaca <69495014+gabrieltanaca@users.noreply.github.com>
2021-09-12 01:58:53 -0300

    🍱 Add-paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
+import { TestText, Global } from '../pages/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Global>
+      <div>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/> 
+      </div>
+      
+      <TestText>
+        <p>Hello App!</p>
+      </TestText>
+      <Component {...pageProps} />
+    </Global>
+  )
 }
 export default MyApp
---
+++ src/pages/global.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Global = styled.div`
+  
+`;
+
+export const TestText = styled.div`
+  height: 97vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p{
+    font-family: Poppins;
+    font-size: 30px ;
+    color: #f72585;
+  }
+`;
